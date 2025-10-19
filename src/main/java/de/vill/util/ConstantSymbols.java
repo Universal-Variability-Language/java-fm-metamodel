6e152f5052011e0643b2d9cdb2ab9c8d2203c7f6
Julia Pham <j.pham@tu-braunschweig.de>
2025-10-19 23:08:51 +0200

    chore: added ConstantSymbols class

---
+++ src/main/java/de/vill/util/ConstantSymbols.java
@@ -0,0 +1,20 @@
+package de.vill.util;
+
+public class ConstantSymbols {
+
+    // Logical operators
+    public static final String OR = "|";
+    public static final String AND = "&";
+    public static final String NOT = "!";
+    public static final String IMPLIES = "=>";
+    public static final String EQUIVALENT = "<=>";
+
+    //Comparison
+    public static final String EQUALS = "=";
+    public static final String NOT_EQUALS = "!=";
+    public static final String GREATER = "<";
+    public static final String LESS = ">";
+    public static final String GREATER_OR_EQUAL = "<=";
+    public static final String LESS_OR_EQUAL = ">=";
+
+}
